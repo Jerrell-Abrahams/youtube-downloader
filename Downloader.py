7f120fd80fa5778ec966a4318c6c94b3114c4607
Jerrell Abrahams <jerrellabrahams50@gmail.com>
2022-09-28 22:26:22 +0200

    Updated GUI

--- Downloader.py
+++ Downloader.py
@@ -1,15 +1,22 @@
+import time
 from pytube import YouTube
 
 class Downloader:
-    
+
     def __init__(self, link) -> None:
         self.link = link
+        
+
+        # def progress_function(stream, chunk, bytes_remaining):
+        #     size = stream.filesize
+        #     progress += (float(abs(bytes_remaining-size)/size))*float(100)
+
         try:
             self.yt = YouTube(self.link)
+            # self.yt.register_on_progress_callback(progress_function)
 
         except (Exception):
-            print("Could not get video")
-
+            self.yt = None
 
     def download_video(self, location, resolution):
         self.yt.streams.filter(res=str(resolution)).first().download(output_path=location)
@@ -26,14 +33,11 @@ class Downloader:
 
     def download_audio_only(self, location):
         self.yt.streams.filter(only_audio=True).first().download(output_path=location)
-        
-
-
     
 
-    
-
-    
-
-
-
+    def percent(self):
+        while True:
+            time.sleep(1)
+            if self.progress == 100:
+                break
+            return self.progress
